787fd6a868a349ee0846754c30de5e5a5b046671
KhanShelby <afkhan_1@hotmail.com>
2024-11-05 23:33:17 +0700

    delete some nav

--- cashier-project/src/Sidebar.js
+++ cashier-project/src/Sidebar.js
@@ -13,10 +13,6 @@ function Sidebar() {
           <span>หน้าหลัก/แคชเชียร์</span>
         </a>
         <a href="#" className="nav-item">
-          <img src="reservation.png" alt="Res Icon" />
-          <span>จองโต๊ะล่วงหน้า</span>
-        </a>
-        <a href="#" className="nav-item">
           <img src="Bill.png" alt="Bill Icon" />
           <span>รายการบิล</span>
         </a>
